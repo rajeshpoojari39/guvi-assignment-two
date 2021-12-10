7c786f01719b5d04b1704b69c9ace16a7f6eda22
Rajesh Poojari <54307904+rajeshpoojari39@users.noreply.github.com>
2021-12-10 21:51:28 +0530

    Update primeNumber.js

--- primeNumber.js
+++ primeNumber.js
@@ -2,18 +2,23 @@ var arr = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
 
-for (let i = 0; i < arr.length; i++) {
-  if (isPrime(arr[i]) == true) {
-    console.log(arr[i]);
-  }
-}
-
-function isPrime(number) {
-  for (let j = 2; j <= number; j++) {
-    if (number % j == 0) {
-      return false;
-    } else {
-      return true;
+function isPrime(arr) {
+  var primeNos = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 2) {
+      var prime = true;
+      for (k = 2; k < arr[i]; k++) {
+        if (arr[i] % k === 0) {
+          prime = false;
+        }
+      }
+      if (prime) {
+        primeNos.push(arr[i]);
+      }
     }
   }
+  return primeNos;
 }
+
+console.log(isPrime(arr));
+
